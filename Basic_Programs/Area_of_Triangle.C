ecea749d6f5bf9d12b5766884aeb61282b0a4058
Ayush Dewangan <122884291+AyushDewangan21@users.noreply.github.com>
2024-05-06 22:27:32 +0530

    Create Area_of_Triangle.C

---
+++ Basic_Programs/Area_of_Triangle.C
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+int main()   
+{ 
+  float b ,h, area;  
+  b= 5; 
+  h= 13;  
+  area = (b*h) / 2 ;  
+  
+   printf("\n Area of Triangle is: %f",area);  
+   return (0);  
+}  
