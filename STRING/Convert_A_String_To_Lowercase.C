fb32705b22b653c0c8755699cb83f4a978457826
Ayush Dewangan <122884291+AyushDewangan21@users.noreply.github.com>
2024-05-08 15:36:42 +0530

    Create Convert_A_String_To_Lowercase.C

---
+++ STRING/Convert_A_String_To_Lowercase.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter any string: ");
+    gets(str);
+
+
+    // Iterate loop till last character of string
+    for(i=0; str[i]!='\0'; i++)
+    {
+        if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i] = str[i] + 32;
+        }
+    }
+
+    printf("Lower case string: %s", str);
+
+    return 0;
+}
